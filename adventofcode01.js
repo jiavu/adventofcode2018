3f8363a6fbbccb878546e29b3e908ff320a058af
jiavu <customerservice@CSGermanyiMac3.fritz.box>
2018-12-04 21:53:55 +0100

    Create adventofcode01.js

---
+++ adventofcode01.js
@@ -0,0 +1,78 @@
+/*
+Import jQuery for Console:
+
+var jq = document.createElement('script');
+jq.src = "https://code.jquery.com/jquery-3.1.1.min.js";
+jq.onload = function(){ 
+	jQuery.noConflict();
+}
+document.getElementsByTagName('head')[0].appendChild(jq);
+
+=============================================================
+
+Data isn't in json format, so this didn't work:
+
+const url = "https://adventofcode.com/2018/day/1/input";
+
+$.getJSON(url, function(result) {
+    console.log(typeof result);
+    console.log(result);
+});
+
+*/
+
+/*
+for refreshing knowledge:
+https://medium.freecodecamp.org/here-is-the-most-popular-ways-to-make-an-http-request-in-javascript-954ce8c95aaa
+
+=============================================================
+*/
+
+// https://adventofcode.com/2018/day/1#part2
+
+
+const Http = new XMLHttpRequest();
+const url='https://adventofcode.com/2018/day/1/input';
+
+Http.open("GET", url);
+Http.send();
+Http.onreadystatechange = function(e){
+    if (this.readyState === XMLHttpRequest.DONE) {
+        let resp = this.response;   //let resp = this.responseText; (both returning a string.)
+
+        const frqChangeList = resp.split("\n");
+        //let frqChangeListInt = [];     // not neccessary
+        let result = 0;
+        let newFrqs = [];
+        let reachedTwice;
+
+        // get resulting frequency:
+        for (let i=0; i < frqChangeList.length-1; i++) {
+            let a = parseInt(frqChangeList[i]);             //or: let a = Number(frqChangeList[i]);
+            //frqChangeListInt.push(a);  // not neccessary
+            result += a;
+            newFrqs.push(result);
+        }
+
+        // get 1st frequency reached twice:
+        for (let i=0; i < newFrqs.length-1; i++) {
+
+            for (let j=0; j < newFrqs.length-1; j++) {
+
+                console.log(newFrqs[j]);
+
+                if (i == j ) continue;
+                else if (newFrqs[i] == newFrqs[j]) {
+                    reachedTwice = newFrqs[j];
+                    console.log("break");
+                    break;
+                }
+            }
+
+            if (reachedTwice) break;
+        }
+
+        console.log("Resulting frequency: " + result);
+        console.log("1st frequency reached twice: " + reachedTwice);
+    }
+};
